7b6d7269fe5249c33c953c728bc3147e41c6cb95
jackson <jacksonjjacobs@gmail.com>
2024-05-23 17:57:26 -0400

    added cockroach setup and reflect db reads in UI

--- client/src/components/wsiViewHook.tsx
+++ client/src/components/wsiViewHook.tsx
@@ -1,11 +1,51 @@
 import React, { useEffect, useState, useRef } from 'react';
+
 // @ts-ignore
 import geo from "geojs";
-import {monitorEventLoopDelay} from "node:perf_hooks";
+
+type Annotation = {
+    type: string,
+    geometry: {
+        type: string,
+        coordinates: number[][][],
+        properties: {
+            color: number[],
+            name: string
+        }
+    }
+}
+
+type Centroid = {
+    type: string,
+    geometry: {
+        type: string,
+        coordinates: number[],
+        properties: {
+            color: number[],
+            name: string
+        }
+    }
+}
+
+interface FeatureCollectionResponse {
+    type: string;
+
+}
+
+interface AnnotationCollection extends FeatureCollectionResponse {
+    features: Annotation[];
+}
+
+interface CentroidCollection extends FeatureCollectionResponse {
+    features: Centroid[];
+}
+
+
 
 const WsiView = () => {
     const viewRef = useRef(null);
-    const [mode, setMode] = useState('polygon');
+    const [annotations, setAnnotations] = useState<AnnotationCollection | null>(null);
+    const [map, setMap] = useState<geo.map | null>(null);
 
     function getRandomInt(min: number, max: number) {
         min = Math.ceil(min); // Ensures the minimum is rounded up to the nearest integer
@@ -20,9 +60,26 @@ const WsiView = () => {
         });
     }
 
+    async function getAnnotations(minx: number, miny: number, maxx: number, maxy: number) {
+        fetch(`http://localhost:5005/image/annotations?minx=${minx}&miny=${miny}&maxx=${maxx}&maxy=${maxy}`)
+            .then(res => res.json())
+            .then((data: AnnotationCollection) => {
+            setAnnotations(data);
+        });
+    }
+
+    // async function getCentroids(minx: number, miny: number, maxx: number, maxy: number) {
+    //     fetch(`http://localhost:5005/image/centroids?minx=${minx}&miny=${miny}&maxx=${maxx}&maxy=${maxy}&centroid`)
+    //         .then(res => res.json())
+    //         .then((data: FeatureCollectionResponse) => {
+    //         setAnnotations(data);
+    //     });
+    // }
+
 
     useEffect(() => {
         const initializeMap = async () => {
+
             const imageServer = 'http://localhost:5005';
             const tileinfo = await fetch(`${imageServer}/image/tile/info`).then(res => res.json());
 
@@ -30,28 +87,16 @@ const WsiView = () => {
             const params = geo.util.pixelCoordinateParams(
                 viewRef.current, tileinfo.sizeX, tileinfo.sizeY, tileinfo.tileWidth, tileinfo.tileHeight);
             const map = geo.map(params.map);
+            setMap(map);
             params.layer.url = `${imageServer}/image/tile/{z}/{x}/{y}`;
-            map.createLayer('osm', params.layer)
 
-            // Create the annotation layer
+            map.createLayer('osm', params.layer)
             map.createLayer('annotation', {clickToEdit: false}).mode('polygon', undefined);
 
+            map.geoOn(geo.event.mousemove, function (evt: any) {
+                console.log(evt.geo.x.toFixed(6), evt.geo.y.toFixed(6));
+            });
 
-            // Create the ground truth layer
-            const pointOptions = {
-                style: {
-                    stroke: true,
-                    strokeColor: 'black',
-                    strokeWidth: 1,
-                    fill: true,
-                    radius: 1,
-                }
-            }
-
-            const groundTruthLayer = map.createLayer('feature', {features: ['point']});
-            const testFeature = groundTruthLayer.createFeature('point', pointOptions);
-            const data = createRandomData(10000000, 5000, 15000, 5000, 15000);
-            testFeature.data(data).draw();
 
             return null;
         };
@@ -60,11 +105,64 @@ const WsiView = () => {
     }, []);
 
 
+    // Create a feature layer when the annotations are loaded
+    useEffect(() => {
+        const initializeFeatureLayer = async () => {
+            if (!annotations) {
+                await getAnnotations(30000, 30000, 40000, 40000);
+            } else {
+                console.log(annotations)
+            }
+
+            if (map && annotations) {
+                // const pointOptions = {
+                //     style: {
+                //         stroke: true,
+                //         strokeColor: 'black',
+                //         strokeWidth: 1,
+                //         fill: true,
+                //         radius: 1,
+                //     }
+                // }
+
+                const layer = map.createLayer('feature', {features: ['polygon', 'point']})
+                var polygons = layer.createFeature('polygon', {selectionAPI: true});
+
+
+                polygons
+                    .data(annotations.features.map(f => f.geometry))
+                    .polygon(function (d: any) {
+                        return {
+                            outer: d.coordinates[0]
+                        };
+                    })
+                    .position((d: number[]) => {
+                        return {x: d[0], y: d[1]}
+                    })
+                    .style((d: Annotation) => {
+                        return {
+                            fill: true,
+                            fillColor: `rgb(${d.geometry.properties.color.join(',')})`,
+                            stroke: true,
+                            strokeColor: 'black',
+                            strokeWidth: 1,
+
+                        }
+                    })
+                    .draw();
+            }
+        }
+
+        initializeFeatureLayer().then(r => console.log('Feature layer initialized'));
+        // testFeature.data(data).draw();
+    }, [annotations, map]);
+
+
     return (
         <div ref={viewRef} style={
             {
-                width: '1000px',
-                height: '1000px',
+                width: '95vw',
+                height: '95vh',
                 position: 'relative',
                 backgroundColor: 'black',
                 top: 0,
